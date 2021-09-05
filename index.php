c60716717c16c32de12442599d0ac1083ca8d237
khalilHnk <79371578+khalilHnk@users.noreply.github.com>
2021-09-05 21:01:03 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Main Page</title>
+  </head>
+  <body>
+    <div>
+      <header>
+        <h1>PAGE TITLE</h1>
+      </header>
+      <main>
+        <article>
+          <h2>AUTHENTIFICATION</h2>
+          <form method="post" action="./test.php">
+              <label for="psword">PASSWORD :</label>
+              <input type="password" id="psword" name="psword">
+              <input type="submit" value="TEST">
+          </form>
+        </article>
+      </main>
+      <footer>
+        <p>Basic Page&copy;2021</p>
+      </footer>
+    </div>
+  </body>
+</html>
